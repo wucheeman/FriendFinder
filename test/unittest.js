9cbef14a36f26118245fb2978f96746c3eb8f3a7
Mark Hainline <wucheeman@gmail.com>
2018-05-09 16:19:40 -0400

    initial commit; contains unit tests for compatibility logic

---
+++ test/unittest.js
@@ -0,0 +1,25 @@
+const assert = chai.assert;
+  
+describe('Scoring', function() {
+  const test_friend = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+  it('sum of scores', function() {
+    const absValComparedScores = [];
+    const tfCompatScore = sumCompatScores(test_friend);
+    assert.equal(tfCompatScore, 32);
+  });
+  it('differenceArray values with all-0 array', function() {
+    const surveyResult = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+    const differenceArray = makeDifferenceArray(surveyResult, test_friend);
+    for (let i = 0; i < differenceArray.length; i++) {
+      assert.equal(differenceArray[i], test_friend[i]);
+    }
+  });
+  it('differenceArray values with ascending/descending array', function() {
+    const surveyResult =    [1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
+    const expectedResults = [4, 1, 1, 0, 0, 4, 2, 2, 2, 0];
+    const differenceArray = makeDifferenceArray(surveyResult, test_friend);
+    for (let i = 0; i < differenceArray.length; i++) {
+      assert.equal(differenceArray[i], expectedResults[i]);
+    }
+  })
+});
