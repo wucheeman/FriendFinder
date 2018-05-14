186c9f8944468e57aebb12a9e30ca452c2b61db8
Mark Hainline <wucheeman@gmail.com>
2018-05-14 11:26:30 -0400

    initial code, plus route to home.html

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 // @ts-check
 // This turns on type checking in VS Code
+
+// DEPENDENCIES
+// =============================================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+// built-in module
+var path = require('path');
+
+// EXPRESS CONFIGURATION
+// =============================================================================
+
+const app = express();
+// Sets an initial port -- Heroku's choice, or default
+let PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES
+// =============================================================================
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './app/public/home.html'));
+  console.log(path.resolve(__dirname, './app/public/index.html'));
+});
+
+// TODO: add other routes
+
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
