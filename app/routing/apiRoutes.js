6de001e05b544c68117aedfe2958e7a402ad6cac
Mark Hainline <wucheeman@gmail.com>
2018-05-14 19:13:44 -0400

    apiRoutes.js updated with POST route to

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,5 +15,16 @@ module.exports = function(app) {
   app.get('/api/friends', function (req, res) {
     res.json(friends);
     console.log('Sending friends data');
-    });
+  });
+
+  app.post('/api/friends', function (req, res) {
+    const {
+      name,
+      photo,
+      scores,
+    } = req.body;
+    // TODO: process request and act accordingly
+    console.log(req.body);
+    res.send(true);
+  }); // end of app.post
 }; // end of module.exports
